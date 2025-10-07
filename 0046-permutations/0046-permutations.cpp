ac7a69cc362333b7f8f6e06cb1f9e24be652ca0f
KritiJadli <138752434+KritiJadli@users.noreply.github.com>
2025-10-07 14:41:26 +0530

    Time: 0 ms (100%), Space: 10.6 MB (69.27%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-void getPerms(vector<int>& nums,vector<vector<int>>&ans,int indx){
+void getPerms(int indx,vector<int>& nums,vector<vector<int>> &ans){
     if(indx==nums.size()){
         ans.push_back({nums});
         return;
     }
     for(int i=indx;i<nums.size();i++){
         swap(nums[i],nums[indx]);
-        getPerms(nums,ans,indx+1);
+        getPerms(indx+1,nums,ans);
         swap(nums[i],nums[indx]);
+
     }
+
 }
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>>ans;
-        getPerms(nums,ans,0);
+        vector<vector<int>> ans;
+        getPerms(0,nums,ans);
         return ans;
+
+
 
     }
 };
