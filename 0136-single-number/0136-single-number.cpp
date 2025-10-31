14d0861dc35c56f684380b1be4988268c03df49e
KritiJadli <138752434+KritiJadli@users.noreply.github.com>
2025-11-01 00:26:55 +0530

    Time: 7 ms (19.98%), Space: 23.8 MB (12.2%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -4,13 +4,12 @@ public:
         unordered_map<int,int>mp;
         for(auto it:nums){
             mp[it]++;
-
         }
         for(auto &[key,value]:mp){
             if(value==1){
                 return key;
             }
         }
-        return 0;
+        return -1;
     }
 };
