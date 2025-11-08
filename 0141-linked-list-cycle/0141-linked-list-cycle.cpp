0a6b6ff9c3001323dbbfd58916f5b5e852a1f2b8
KritiJadli <138752434+KritiJadli@users.noreply.github.com>
2025-11-08 19:17:59 +0530

    Time: 12 ms (38.15%), Space: 11.9 MB (52.24%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -12,9 +12,9 @@ public:
         ListNode* slow=head;
         ListNode* fast=head;
         while(fast!=NULL && fast->next!=NULL){
-            fast=fast->next->next;
             slow=slow->next;
-            if(fast==slow)return true;
+            fast=fast->next->next;
+            if(slow==fast)return true;
         }
         return false;
     }
