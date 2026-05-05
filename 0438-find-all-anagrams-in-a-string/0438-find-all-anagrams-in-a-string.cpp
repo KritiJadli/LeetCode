8f447273fe0be3eab4c484e1ce5b8badbe7a0090
KritiJadli <138752434+KritiJadli@users.noreply.github.com>
2026-05-05 20:21:38 +0530

    0438-FIND-ALL-ANAGRAMS-IN-A-STRING Stats: Time: 3 ms (78.73%), Space: 11.4 MB (97.3%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,39 +1,35 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        vector<int>freq(26,0);
-        for(char c:p){
-            freq[c-'a']++;
-        }
-        int count=p.size();
-        int k=p.size();
-        vector<int>res;
-        int i=0;
-        int j=0;
-        while(j<s.size()){
-            if(freq[s[j]-'a']>0){
+        vector<int>vec(26,0);
+            for(char c:p){
+                vec[c-'a']++;
+            }
+           int i=0;
+           int j=0;
+           int count=p.size();
+           int k=p.size();
+           vector<int>res;
+           while(j<s.size()){
+            if(vec[s[j]-'a']>0){
                 count--;
             }
-            freq[s[j]-'a']--;
-
-            if(j-i+1<k){
-                j++;
-            }
+            vec[s[j]-'a']--;
+            if(j-i+1<k)j++;
             else if(j-i+1==k){
                 if(count==0){
-                    res.push_back(i);    
+                    res.push_back(i);
                 }
-                if(freq[s[i]-'a']>=0){
+                if(vec[s[i]-'a']>=0){
                     count++;
                 }
-                freq[s[i]-'a']++; 
-                i++;
-                j++;
+                vec[s[i]-'a']++;
+                 i++;
+                 j++;
+            }
            
-            } 
-            
-            
-        }
+           }
+        
         return res;
     }
 };
