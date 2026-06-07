10a84de1197c4a576ccc6e705cb0c99100175236
KritiJadli <138752434+KritiJadli@users.noreply.github.com>
2026-06-07 18:23:36 +0530

    0001-TWO-SUM Stats: Time: 0 ms (100%), Space: 14.8 MB (54.57%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,15 +3,12 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int>mp;
         for(int i=0;i<nums.size();i++){
-            int complement=target-nums[i];
-            if(mp.find(complement)!=mp.end()){
-                return {mp[complement],i};
-
+            int comp=target-nums[i];
+            if(mp.find(comp)!=mp.end()){
+                return {i,mp[comp]};
             }
             mp[nums[i]]=i;
-
         }
         return {};
-        
     }
 };
