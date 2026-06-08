be012bfaf5555c0198f9f639f4698126566f1929
KritiJadli <138752434+KritiJadli@users.noreply.github.com>
2026-06-08 07:04:05 +0530

    0083-REMOVE-DUPLICATES-FROM-SORTED-LIST Stats: Time: 0 ms (100%), Space: 16.1 MB (67.38%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -11,15 +11,14 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        if(!head)return NULL;
         ListNode* curr=head;
-        while(curr->next!=NULL){
-        if(curr->val==curr->next->val){
-            curr->next=curr->next->next;
-        }
-        else{
-        curr=curr->next;
-        }
+        while(curr && curr->next){
+            if(curr->val==curr->next->val){
+                curr->next=curr->next->next;
+            }
+            else{
+                curr=curr->next;
+            }
         }
         return head;
     }
