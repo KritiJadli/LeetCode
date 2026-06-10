c3da746c18e962e80ed09e7472a42720ae7bbb80
KritiJadli <138752434+KritiJadli@users.noreply.github.com>
2026-06-10 21:54:44 +0530

    0283-MOVE-ZEROES Stats: Time: 0 ms (100%), Space: 24 MB (18.28%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-       int temp=0;
-       for(int i=0;i<nums.size();i++){
-        if(nums[i]!=0){
-            nums[temp]=nums[i];
-            temp++;
+        int temp=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                nums[temp]=nums[i];
+                temp++;
+
+            }
+
         }
-       }
-       for(int i=temp;i<nums.size();i++){
+        for(int i=temp;i<nums.size();i++){
+            if(nums[i]==0);
             nums[i]=0;
-       }
-    
+        }
+        
     }
 };
