595c9383936dfeb6fcb2de11f32b3183e5abbf6d
KritiJadli <138752434+KritiJadli@users.noreply.github.com>
2026-08-02 23:42:35 +0530

    Time: 829 ms (20.21%), Space: 11.7 MB (29.18%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,38 +1,43 @@
 class Solution {
 public:
-    vector<pair<int,int>>dir={{1,0},{-1,0},{0,1},{0,-1}};
-    int m,n;
-    bool find(vector<vector<char>>& board,string& word,int i,int j,int indx) {
-        if(indx==word.size()){
+vector<vector<int>> directions{{1,0},{-1,0},{0,1},{0,-1}};
+    bool find(int i,int j,int idx,string word,vector<vector<char>>& board){
+        if(idx==word.length()){
             return true;
         }
-        if(i<0||j<0|| i>=board.size() || j>=board[0].size() || board[i][j]!=word[indx])return false;
-        int temp=board[i][j];
+        if(i<0 || j<0 || i>=board.size() || j>=board[0].size() || board[i][j]=='$'){
+            return false;
+        }
+        if(board[i][j]!=word[idx])return false;
+        char temp=board[i][j];
         board[i][j]='$';
-        for(auto &it:dir){
-            int new_i=i+it.first;
-            int new_j=j+it.second;
-            if(find(board,word,new_i,new_j,indx+1)){
-                board[i][j]=temp;
+        for(auto &dir : directions){
+            int new_i=i+ dir[0];
+            int new_j=j+ dir[1];
+            if(find(new_i,new_j,idx+1,word,board)){
                 return true;
             }
         }
         board[i][j]=temp;
-                return false;
+        return false;
+
 
     }
     bool exist(vector<vector<char>>& board, string word) {
-         m=board.size();
-         n=board[0].size();
+        int m= board.size();
+        int n= board[0].size();
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(board[i][j]==word[0] && find(board,word,i,j,0)){
+                if(board[i][j]==word[0] && find(i,j,0,word,board)){
                     return true;
-                }
 
+                }
             }
         }
         return false;
-
     }
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
